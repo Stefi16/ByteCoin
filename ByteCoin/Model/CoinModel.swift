be55e8f4fdfd0acfdf076bb954b826a0c55c6dba
Stefi16 <67965028+Stefi16@users.noreply.github.com>
2024-06-16 00:26:34 +0300

    Edit data

--- ByteCoin/Model/CoinModel.swift
+++ ByteCoin/Model/CoinModel.swift
@@ -1,11 +1,3 @@
-//
-//  CoinModel.swift
-//  ByteCoin
-//
-//  Created by Stefka Krachunova on 12.06.24.
-//  Copyright © 2024 The App Brewery. All rights reserved.
-//
-
 import Foundation
 
 struct CoinModel: Decodable {
